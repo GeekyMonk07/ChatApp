3b32db35382d27e1ea77fcee5d7112509c3461db
GeekyMonk07 <thelearninggeek07@gmail.com>
2021-07-01 12:08:29 +0530

    In Progress

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:chat_app/login_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: LoginScreen(),
+    );
+  }
+}
